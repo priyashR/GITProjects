e83cbba490b94111f728c849a0a182f2b111f144
priyashR <priyash.ramawthar@gmail.com>
2018-09-20 23:25:10 +0200

    dataIngest microservice

---
+++ dataIngest/src/main/java/com/gmail/ramawthar/priyash/dataIngest/DataIngestApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.ramawthar.priyash.dataIngest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataIngestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataIngestApplication.class, args);
+	}
+}
