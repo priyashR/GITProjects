79734e5da423ff8527061169daa40edadf04d307
priyashR <priyash.ramawthar@gmail.com>
2018-09-20 23:40:21 +0200

    test

--- dataIngest/src/main/java/com/gmail/ramawthar/priyash/dataIngest/DataIngestApplication.java
+++ dataIngest/src/main/java/com/gmail/ramawthar/priyash/dataIngest/DataIngestApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DataIngestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataIngestApplication.class, args);
+		SpringApplication.run(DataIngestApplication.class, args) ; 
 	}
 }
