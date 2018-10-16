8f60734475cd84dcb3e7b146aa41ee8fa77ddb40
priyashR <priyash.ramawthar@gmail.com>
2018-10-16 13:10:46 +0200

    testing the gmail api

---
+++ testGmailApi/src/main/java/com/gmail/ramawthar/priyash/testGmailApi/TestGmailApiApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.ramawthar.priyash.testGmailApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGmailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGmailApiApplication.class, args);
+	}
+}
---
+++ testGmailApi/src/test/java/com/gmail/ramawthar/priyash/testGmailApi/TestGmailApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.ramawthar.priyash.testGmailApi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestGmailApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
